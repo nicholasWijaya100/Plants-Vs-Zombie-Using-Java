a428c89302f878e3a093c0532312102162396c7f
timothyjohan <timothyjohan11@gmail.com>
2022-05-23 18:48:39 +0700

    Bucket&Cone
    class & constructor

---
+++ PlantsVsZombie/src/ConeheadZombie.java
@@ -0,0 +1,19 @@
+
+import java.awt.Image;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author user
+ */
+public class ConeheadZombie extends Zombie{
+
+    public ConeheadZombie(int maxHp, Image zombieImage, int x, int y, int damage, double waktuTiapMakan, double terakhirMakan, int kecepatan) {
+        super(maxHp, zombieImage, x, y, damage, waktuTiapMakan, terakhirMakan, kecepatan);
+    }
+    
+}
